467cd2dcd46400af70921705f3c88e0fbb44d0f2
최민호 <alsgh1003@hanmail.net>
2017-09-21 15:54:56 +0900

    login function (global_login_page)

--- frontend/src/app/component/global_login/global_login.component.ts
+++ frontend/src/app/component/global_login/global_login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { PasswordValidation } from './custom_validation.component';
+import { AuthService } from '../../service/auth.service';
+
 
 
 @Component({
@@ -10,9 +13,10 @@ import { PasswordValidation } from './custom_validation.component';
 
 export class GlobalLoginComponent{
   form: FormGroup;
+  model: any = {};
 
 
-  constructor(fb: FormBuilder)
+  constructor(private router: Router, private authService: AuthService, fb: FormBuilder)
   {
     this.form = fb.group({
       password: ['', Validators.required],
@@ -20,6 +24,19 @@ export class GlobalLoginComponent{
     }, {
       validator: PasswordValidation.MatchPassword // your validation method
     })
+  }
+
+  login() {
+    this.authService.login(this.model.email, this.model.password)
+      .subscribe(
+        data => {
+          this.router.navigateByUrl('/home');
+          location.reload();
+        },
+        error => {
+          alert('User does note exist!')
+        },
+      );
   }
 
 
