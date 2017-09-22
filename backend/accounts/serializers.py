47e609618cb4471de510d6bbee87f4c621b052f2
최민호 <alsgh1003@hanmail.net>
2017-09-22 10:49:09 +0900

    fix token-auth credentials problem

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
-from .models import  User
+from .models import User
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'password', 'is_superuser', 'first_name', 'last_name', 'address', 'contact_num', 'date_joined')
+        fields = ('id', 'email', 'password', 'is_superuser', 'first_name', 'last_name', 'address', 'contact_num', 'date_joined', 'is_active')
+
+        # store hashing password
+        def create(self, validated_data):
+            password = validated_data.pop('password', None)
+            instance = self.Meta.model(**validated_data)
+            if password is not None:
+                instance.set_password(password)
+            instance.save()
+            return instance
 
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # Admin site
     
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     'mall',
     'accounts',
