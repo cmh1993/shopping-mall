bac45be6a4a04a5e083c813c5057d5f8073d4857
최민호 <alsgh1003@hanmail.net>
2017-09-22 12:07:27 +0900

    Override to return a custom response such as including the serialized representation of the User.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -45,10 +46,12 @@ INSTALLED_APPS = [
     'men',
 ]
 
+
 # corsheaders
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+
 
 # JSONWebTokenAuthentication to Django REST framework's DEFAULT_AUTHENTICATION_CLASSES
 
@@ -63,8 +66,52 @@ REST_FRAMEWORK = {
     ),
 }
 
+
 # Custom user model
+
 AUTH_USER_MODEL = 'accounts.User'
+
+
+# djangorestframework-jwt settings
+
+JWT_AUTH = {
+    # My Definition jwt override file
+    'JWT_RESPONSE_PAYLOAD_HANDLER':
+        'utils.jwt.jwt_response_payload_handler',
+
+    # Basic settings
+    'JWT_ENCODE_HANDLER':
+        'rest_framework_jwt.utils.jwt_encode_handler',
+
+    'JWT_DECODE_HANDLER':
+        'rest_framework_jwt.utils.jwt_decode_handler',
+
+    'JWT_PAYLOAD_HANDLER':
+        'rest_framework_jwt.utils.jwt_payload_handler',
+
+    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+        'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+
+    'JWT_SECRET_KEY': SECRET_KEY,
+    'JWT_GET_USER_SECRET_KEY': None,
+    'JWT_PUBLIC_KEY': None,
+    'JWT_PRIVATE_KEY': None,
+    'JWT_ALGORITHM': 'HS256',
+    'JWT_VERIFY': True,
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_LEEWAY': 0,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300),
+    'JWT_AUDIENCE': None,
+    'JWT_ISSUER': None,
+
+    'JWT_ALLOW_REFRESH': False,
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+
+    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+    'JWT_AUTH_COOKIE': None,
+
+}
 
 
 MIDDLEWARE = [
