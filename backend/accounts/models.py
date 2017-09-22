6434684d4b474af6a8aa2b55afb9a13ef2d93f2e
최민호 <alsgh1003@hanmail.net>
2017-09-22 12:09:36 +0900

    no message

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -5,9 +5,7 @@ from django.core.mail import send_mail
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
-
 from .managers import UserManager
-
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.hashers import make_password
 from .models import User
 from .serializers import UserSerializer
 
-
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
